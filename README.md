068b26fcd7ade2a02566f0c0c48ab1ff42f5c34f
Erwin Dondorp <erwin@dondorp.com>
2018-04-08 20:40:30 +0200

    Initial release

---
+++ README.md
@@ -0,0 +1,100 @@
+
+### Byron SX series doorbells
+Homey app to receive signals from Byron SX doorbell pushbuttons; and to send signal to Byron SX doorbell chimes.
+
+These devices are supported:
+
+* Doorbell push buttons:
+	SX30 tested
+	SX32 
+	SX33
+	SX35
+	SX38
+	SX39
+
+* Doorbell chimes:
+	SX1i
+	SX4i
+	SX5i
+	SX6
+	SX7
+	SX8i
+	SX11i
+	SX14
+	SX15
+	SX15i
+	SX21
+	SX20
+	SX34i
+	SX40i
+	SX41i
+	SX55
+	SX56
+	SX60i
+	SX72
+	SX80
+	SX81
+	SX83
+	SX86
+	SX102
+	SX120
+	SX201
+	SX202
+	SX204
+	SX205
+	SX209
+	SX210i
+	SX212i
+	SX220
+	SX222
+	SX225
+	SX226
+	SX228
+	SX245
+	SX252
+	SX823
+Depending on the model, the list of possible sounds is adjusted.
+
+### Installation
+Install the app and add the App to a flow. 
+It can be added as a starting trigger, or as an action.
+
+#### Activity
+
+When the card is added to the condition column, it detects a Byron SX push button being pushed. 3 parameters are added to the trigger:
+* ringerId
+Contains the internal ID of the ringer that is pushed. This is a number between 0 and 255. This number is assigned randomly to a push button once the battery is inserted.
+* melodyId
+Contains the internal ID of the melody that was chosen. This is a number between 0 and 15. But only 8 specific values seem to in use. This number can be changed by pushing the small button that is inside the push button.
+* melodyNr
+Contains the melody number of the melody that was chosen. This is a number between 1 and 8. These are the numbers that are listed in the manual that comes with the device. Some bells only support 4 melodies. Some bells support only 1 melody or are mechanical bells.
+
+When the card is added to the action column, it is able to send a signal to a Byron SX bell. There are separate cards for each category of bell. There are 6 categories. A 7th card is available, which allows all 16 melody IDs. The hint icon shows the exact models that are supported by that card. But you can also pick the card that has the list of ringtones that matches your Byron SX bell. This card takes a few parameters:
+* ringerId
+Contains the internal ID of the bell that is addressed. This is a number between 0 and 255. Typically, this is the same number as is sent by the push button.
+* melodyNr
+Contains the melody that should be played by the bell. You can select the melody from a dropdown list. When you select the proper card, the list of melodies should exactly match with what is possible. This parameter is not available for bells that have only one melody.
+* melodyId
+Contains the melody that should be played by the bell. You can select the melody by its internal number. Not that the internal numbers are not equal to the sequence number suggested by the dropdown lists.
+
+You may create more complicated scenarios where you let the Homey flow decide whether or not to send a command to the bell. First pair be bell so that it has a known internal ID. Then remove the batteries from the push button and reinsert them. This gives the push button a new random internal ID. Unless you are unlucky, the push button now has a different internal ID than the bell. You can use this to let your Homey decide whether the bell should indeed ring.
+
+#### Protocols
+The Byron SX series push buttons and bells all use the same 433MHz-based protocol. It consists of a simple message consisting of 8 bits for the push button identification and 4 bits for the melody identification. This would suggest 256 possible IDs and 16 possible ringtones. Depending on the models of your push button and bell, less combinations may be in use.
+
+### Discussion and issues
+Please report issues via [GitHub](https://github.com/erwindon/com.dondorp.homey.byronsx.git/issues). For other remarks and/or questions mail byronsx@dondorp.com.
+
+### Version info
+* 1.0.0 First Athom App Store release
+
+----------
+
+
+Copyright (c) 2018 Erwin Dondorp
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ app.js
@@ -0,0 +1,338 @@
+// Homey App for Byron SX series doorbells
+
+// Author: Erwin Dondorp
+// E-Mail: byronsx@dondorp.com
+
+// PUSH BUTTONs
+// ============
+// SX30 8 codes, tested
+// SX32 8 codes
+// SX33 8 codes
+// SX35 8 codes
+// SX38 8 codes
+// SX39 8 codes
+
+// DOOR CHIMEs
+// ===========
+// not that we are not supporting the chimes now
+// i.e. the Homey App currently cannot ring the bell
+// SX1i   8 melodies A
+// SX4i   8 melodies A
+// SX5i   mechanical E
+// SX6    4 melodies B
+// SX7    4 melodies B
+// SX8i   8 melodies A
+// SX11i  8 melodies A
+// SX14   8 melodies A
+// SX15   mechanical E
+// SX15i  mechanical E
+// SX21   4 melodies B
+// SX20   8 melodies A
+// SX34i  8 melodies A
+// SX40i  8 melodies A
+// SX41i  8 melodies A
+// SX55   8 melodies A
+// SX56   8 melodies A
+// SX60i  mechanical E
+// SX72   8 melodies C
+// SX80   4 melodies B, used in test
+// SX81   8 melodies D
+// SX83   4 melodies F
+// SX86   8 melodies D
+// SX102  8 melodies A
+// SX120  4 melodies B
+// SX201  8 melodies C
+// SX202  8 melodies C (or recorded sound)
+// SX204  8 melodies D
+// SX205  8 melodies A
+// SX209  8 melodies C
+// SX210i 8 melodies A
+// SX212i 8 melodies A
+// SX220  8 melodies A
+// SX222  8 melodies A
+// SX225  8 melodies A
+// SX226  8 melodies A
+// SX228  8 melodies A
+// SX245  8 melodies A
+// SX252  fixed sound E
+// SX823  4 melodies F
+//
+// IN DOUBT
+// ========
+// SX304
+// SX503
+//
+// OTHER SX PRODUCTS
+// =================
+// SX200 batteries
+// SX36 9v A/C adapter
+//
+// 8 melodies A
+// ============
+// 0 = Tubular 3-notes
+// 1 = Big Ben
+// 2 = Tubular 2-notes
+// 3 = Solo
+// 4 = Tubular Scare
+// 5 = Clarinet
+// 6 = Saxophone
+// 7 = Morning Dew
+//
+// 4 melodies B
+// ==========
+// 0 = Tubular 3-notes
+// 1 = Big Ben
+// 2 = Tubular 2-notes
+// 3 = Solo
+// (4-7) play as (0-3)
+//
+// 8 melodies C
+// ============
+// 0 = Electro Mechanical - Ding Dong
+// 1 = Westminster - 8 notes
+// 2 = Jive
+// 3 = Wedding March
+// 4 = Piano
+// 5 = Harmony
+// 6 = Dog Barking
+// 7 = Ringer
+//
+// 8 melodies D
+// ============
+// 0 = Westminster
+// 1 = Ding Dong Repeat
+// 2 = Telephone Ring
+// 3 = Circus Theme
+// 4 = Banjo on my Knee
+// 5 = Twinkle Twinkle
+// 6 = It's a Small World
+// 7 = Dog Barking
+
+// TODO
+// 1) need Dutch names for "8 melodies C"
+// 2) need Dutch names for "8 melodies D"
+
+'use strict';
+
+const Homey = require('homey');
+
+var lastRing = Date.now();
+
+// The internal IDs are different from the melody numbers
+// This translation table converts from ID to melody-number
+// Only 8 out of 16 IDs are used
+var melodyNrs = {
+	 0: "0",
+	 1: "1",
+	 2: "8",
+	 3: "2",
+	 4: "0",
+	 5: "3",
+	 6: "7",
+	 7: "0",
+	 8: "0",
+	 9: "4",
+	10: "0",
+	11: "0",
+	12: "0",
+	13: "5",
+	14: "6",
+	15: "0",
+	};
+
+var melodyIds = {
+	"1": 1,
+	"2": 3, 
+	"3": 5, 
+	"4": 9, 
+	"5": 13, 
+	"6": 14, 
+	"7": 6,
+	"8": 2,
+}
+
+let bellPressedTrigger = new Homey.FlowCardTrigger('receive_ring_signal');
+bellPressedTrigger.register();
+
+// create & register a signal using the id from your app.json
+let mySignal = new Homey.Signal433('ByronSxDoorbellSignal');
+mySignal.register()
+	.then(() => {
+		// Analysis for melodyBits:
+		// payload: 0,0,1,0,0,1,0,1,0,0,0,1 Tubular 3-notes
+		// payload: 0,0,1,0,0,1,0,1,0,0,1,1 Big Ben
+		// payload: 0,0,1,0,0,1,0,1,0,1,0,1 Tubular 2-notes
+		// payload: 0,0,1,0,0,1,0,1,1,0,0,1 Solo
+		// payload: 0,0,1,0,0,1,0,1,1,1,0,1 Tubular Scare
+		// payload: 0,0,1,0,0,1,0,1,1,1,1,0 Clarinet
+		// payload: 0,0,1,0,0,1,0,1,0,1,1,0 Saxophone
+		// payload: 0,0,1,0,0,1,0,1,0,0,1,0 Morning Dew
+		// 8 other possible codes in last 4 bits do not seem to be generated
+
+		// Other deviceIds
+		// proves that all first 8 bit are used
+		// payload: 0,1,0,0,0,0,1,1,0,0,0,1
+		// payload: 1,0,1,0,1,0,0,1,0,0,0,1
+		// payload: 1,0,0,0,1,0,1,0,0,0,0,1
+
+		mySignal.on('payload', function(payload, first) {
+			//console.log('received: signal:[' + payload + '], first:' + first);
+
+			// take the relevant groups of bits
+			var ringerBits = payload.slice(0, 8);
+			var melodyBits = payload.slice(8, 12);
+
+			// get the values from the bit-patterns
+			var ringerId =
+				ringerBits[0] * 128 +
+				ringerBits[1] * 64 +
+				ringerBits[2] * 32 +
+				ringerBits[3] * 16 +
+				ringerBits[4] * 8 +
+				ringerBits[5] * 4 +
+				ringerBits[6] * 2 +
+				ringerBits[7] * 1;
+			var melodyId =
+				melodyBits[0] * 8 +
+				melodyBits[1] * 4 +
+				melodyBits[2] * 2 +
+				melodyBits[3] * 1;
+
+			var melodyNr = melodyNrs[melodyId];
+
+			// Controller seems to return multiple events with "first=true"
+			// Therefore we use our own mechanism
+			var now = Date.now();
+			var millis = now - lastRing;
+			if(millis < 5000)
+			{
+				// Accept only one ring within 5 seconds
+				// console.log('IGNORED ringer: [' + ringerBits + ']=' + ringerId + ', melody: [' + melodyBits + ']=' + melodyId + "(" + melodyNr + "), first: " + first);
+				return;
+			}
+
+			lastRing = now;
+
+			console.log('ringer: [' + ringerBits + ']=' + ringerId + ', melody: [' + melodyBits + ']=' + melodyId);
+
+			var tokens = {
+				'ringerId': ringerId,
+				'melodyId': melodyId,
+				'melodyNr': melodyNr
+				};
+
+			bellPressedTrigger
+				.trigger(tokens, tokens)
+				.catch(this.error)
+				.then(this.log);
+		})
+})
+.catch(this.error);
+
+let ringBellActionNrA = new Homey.FlowCardAction('send_ring_signal_nrA');
+ringBellActionNrA.register();
+
+let ringBellActionNrB = new Homey.FlowCardAction('send_ring_signal_nrB');
+ringBellActionNrB.register();
+
+let ringBellActionNrC = new Homey.FlowCardAction('send_ring_signal_nrC');
+ringBellActionNrC.register();
+
+let ringBellActionNrD = new Homey.FlowCardAction('send_ring_signal_nrD');
+ringBellActionNrD.register();
+
+let ringBellActionNrE = new Homey.FlowCardAction('send_ring_signal_nrE');
+ringBellActionNrE.register();
+
+let ringBellActionId = new Homey.FlowCardAction('send_ring_signal_id');
+ringBellActionId.register();
+
+function getBits(ringerId, melodyId)
+{
+	// +256 to force fixed length of 9 bits
+	// then use bits 1..8 (but not bit 0)
+	var ringerIdBits = (ringerId + 256).toString(2);
+	var melodyIdBits = (melodyId + 16).toString(2);
+	return [
+			parseInt(ringerIdBits[1]),
+			parseInt(ringerIdBits[2]),
+			parseInt(ringerIdBits[3]),
+			parseInt(ringerIdBits[4]),
+			parseInt(ringerIdBits[5]),
+			parseInt(ringerIdBits[6]),
+			parseInt(ringerIdBits[7]),
+			parseInt(ringerIdBits[8]),
+			parseInt(melodyIdBits[1]),
+			parseInt(melodyIdBits[2]),
+			parseInt(melodyIdBits[3]),
+			parseInt(melodyIdBits[4])
+			];
+}
+
+class ByronSxDoorbell extends Homey.App {
+	onInit() {
+		ringBellActionNrA.registerRunListener((args, state) => {
+			var ringerId = args['ringerId']
+			var melodyNr = args['melodyNr']
+			this.log('RING-A: ringerId:' + ringerId + ', melodyNr:' + melodyNr);
+			var melodyId = melodyIds[melodyNr];
+			var bits = getBits(ringerId, melodyId);
+			mySignal.tx(bits, console.log);
+			return true;
+		});
+
+		ringBellActionNrB.registerRunListener((args, state) => {
+			var ringerId = args['ringerId']
+			var melodyNr = args['melodyNr']
+			this.log('RING-B: ringerId:' + ringerId + ', melodyNr:' + melodyNr);
+			var melodyId = melodyIds[melodyNr];
+			var bits = getBits(ringerId, melodyId);
+			mySignal.tx(bits, console.log);
+			return true;
+		});
+
+		ringBellActionNrC.registerRunListener((args, state) => {
+			var ringerId = args['ringerId']
+			var melodyNr = args['melodyNr']
+			this.log('RING-C: ringerId:' + ringerId + ', melodyNr:' + melodyNr);
+			var melodyId = melodyIds[melodyNr];
+			var bits = getBits(ringerId, melodyId);
+			mySignal.tx(bits, console.log);
+			return true;
+		});
+
+		ringBellActionNrD.registerRunListener((args, state) => {
+			var ringerId = args['ringerId']
+			var melodyNr = args['melodyNr']
+			this.log('RING-D: ringerId:' + ringerId + ', melodyNr:' + melodyNr);
+			var melodyId = melodyIds[melodyNr];
+			var bits = getBits(ringerId, melodyId);
+			mySignal.tx(bits, console.log);
+			return true;
+		});
+
+		ringBellActionNrE.registerRunListener((args, state) => {
+			var ringerId = args['ringerId']
+			this.log('RING-E: ringerId:' + ringerId);
+			var bits = getBits(ringerId, 0);
+			mySignal.tx(bits, console.log);
+			return true;
+		});
+
+		ringBellActionId.registerRunListener((args, state) => {
+			var ringerId = args['ringerId']
+			var melodyId = args['melodyId']
+			this.log('RING: ringerId:' + ringerId + ', melodyId:' + melodyId);
+			var bits = getBits(ringerId, melodyId);
+			this.log("bits:", bits);
+			mySignal.tx(bits, console.log);
+			return true;
+		});
+
+		this.log('ByronSxDoorbell is running...');
+	}
+}
+
+module.exports = ByronSxDoorbell;
+
+// vim:ts=4
